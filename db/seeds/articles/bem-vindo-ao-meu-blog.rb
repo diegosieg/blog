9cc60a4d158311348734f0c3b9095891130940cb
fabio <fabio.info@gmail.com>
2014-01-01 23:15:08 -0200

    articles text adjustment

--- db/seeds/articles/bem-vindo-ao-meu-blog.rb
+++ db/seeds/articles/bem-vindo-ao-meu-blog.rb
@@ -3,7 +3,7 @@ Article.create! do |article|
   article.title = "Bem-vindo ao meu blog"
   article.published_at = Time.new(2014, 1, 1)
   article.description = <<-HTML
-<p>Meu nome é Fábio Rodrigues, sou programador desde meus 17 anos. Gosto de dizer que nunca trabalhei com nada além disso. Criei este blog para compartilhar um pouco do meu conhecimendo com a comunidade.</p>
+<p>Meu nome é Fábio Rodrigues, sou formado em Ciências da Computação e desenvolvo profissionalmente desde meus 17 anos. Gosto de dizer que nunca trabalhei com nada além disso. Criei este blog para compartilhar um pouco do meu conhecimendo com a comunidade.</p>
 
 <p>Comecei minha carreira programando em Delphi, e assim foi por muitos anos. Também já me aventurei em outros liguagens (Java, .Net, PHP, etc...). Até que um dia conheci Ruby on Rails e não larguei mais.</p>
 
@@ -11,7 +11,7 @@ Article.create! do |article|
 
 <p>Como primeira contribuição, resolvi fazer esse blog do zero e disponibilizar o código-fonte para quem também quiser nos dar o prazer de divulgar seus conhecimentos. Fiquem a vontade para usar... <a href="https://github.com/FabioMR/blog" target="_blank">github.com/FabioMR/blog</a></p>
 
-<p>Farei o possível para sempre estar postando algo novo por aqui.</p>
+<p>Farei o possível para estar sempre postando algo novo por aqui.</p>
 
 <p>Sejam bem-vindos!</p>
   HTML
--- db/seeds/articles/i18n-para-datas-numeros-e-boolianos.rb
+++ db/seeds/articles/i18n-para-datas-numeros-e-boolianos.rb
@@ -1,9 +1,9 @@
 Article.create! do |article|
-  article.slug = "i18n-para-datas-numeros-e-boolianos"
-  article.title = "I18n para datas, números e boolianos"
+  article.slug = "i18n-para-datas-numeros-e-booleanos"
+  article.title = "I18n para datas, números e booleanos"
   article.published_at = Time.new(2014, 1, 6)
   article.description = <<-HTML
-<p>Aqui vai um pequeno truque que uso em todos meus projetos para facilitar a tradução de datas, números e boolianos.</p>
+<p>Aqui vai um pequeno truque que uso em todos meus projetos para facilitar a tradução de datas, números e booleanos.</p>
 
 <p>Crie o arquivo <strong>config/initializers/locale.rb</strong></p>
 <script src="https://gist.github.com/FabioMR/8197230.js"></script>
@@ -14,7 +14,7 @@ Article.create! do |article|
 <p>Traduzindo números...</p>
 <script src="https://gist.github.com/FabioMR/cbe773f1f7671b45e041.js"></script>
 
-<p>Traduzindo boolianos...</p>
+<p>Traduzindo booleanos...</p>
 <script src="https://gist.github.com/FabioMR/5c2acc8c2203e2d691c2.js"></script>
 
 <p>E caso seja um valor nulo...</p>
